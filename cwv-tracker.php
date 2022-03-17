4c801ef3cf9847396380ab21ec50de86567ee490
DimitriNakos <dimi1nakos@gmail.com>
2022-03-17 00:22:50 -0400

    pseudo codin it for now

--- cwv-tracker.php
+++ cwv-tracker.php
@@ -8,6 +8,24 @@
  *
  */
 
+// Build out a settings page.
+
+// Store a favorite post URL.
+
+// Get the site URL.
+
+// add a button to get the GPSI to run the homepage and post.
+
+// Save the GPSI numbers to a variable.
+
+// display the numbers.
+
+// Store the GPSI numbers into the database.
+
+// grab past GPSI numbers from database.
+
+// Display GPSI numbers with dates in a table.
+
 $mobile = 0;
 $desktop = 0;
 $test_post_URL = "";
